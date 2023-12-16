58bcb7574b2254e39a6e49606aee0a119ea48af6
Jayden Turgott <jturgott@terpmail.umd.edu>
2023-12-16 17:59:34 -0500

    Added api and database support

--- code.js
+++ code.js
@@ -4,12 +4,13 @@ const fs = require("fs");
 const express = require('express');
 const app = express();
 const path = require("path");
+const fetch = require("node-fetch");
 require("dotenv").config({ path: path.resolve(__dirname, '.env') }) 
 
 const uri = process.env.MONGO_CONNECTION_STRING;
 const databaseAndCollection = {db: "Final_Proj", collection:"QR"};
-
 const { MongoClient, ServerApiVersion } = require('mongodb');
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
     const bodyParser = require("body-parser");
     app.use(bodyParser.urlencoded({extended:false}));
     if(process.argv.length != 3 ){
@@ -50,6 +51,66 @@ const { MongoClient, ServerApiVersion } = require('mongodb');
     app.get("/", (request, response) => { 
     response.render("form");
     });
+
+    app.post("/result", async (request, response) => {
+
+      html = "";
+      html+=`Here's Your qr for ${request.body.url} <br>`
+      QR=`<img src="https://qrtag.net/api/qr_4.png?url=https://${request.body.url}" alt="qrtag"><br>`
+      html+=QR;
+        try {
+            await client.connect();
+           
+            /* Inserting one movie */
+            console.log("***** Inserting one QR *****");
+            let QR = {name: request.body.name,
+              email: request.body.email,
+              url: request.body.url};
+            await insertQR(client, databaseAndCollection, QR);    
+        } catch (e) {
+            console.error(e);
+        } finally {
+            await client.close();
+        }
+
+        let result = undefined;
+
+    try {
+        await client.connect();
+        result = await lookupQRs(client, databaseAndCollection, request.body.name, request.body.email);
+
+    } catch (e) {
+        console.error(e);
+    } finally {
+        await client.close();
+}
+  let table = "";
+    html+=`All other QR codes from user with name: ${request.body.name}, and email: ${request.body.email}<br>`;
+    for( let entry of result){
+      console.log(entry)
+      table+=`<tr><td>${entry.url}</td><td><img src="https://qrtag.net/api/qr_4.png?url=https://${entry.url}" alt="qrtag"><br></td></tr>`
+    }
+    html+=table;
+    
+      response.send(html);
+
+})
+
+async function insertQR(client, databaseAndCollection, QR) {
+  const result = await client.db(databaseAndCollection.db).collection(databaseAndCollection.collection).insertOne(QR);
+
+  console.log(`QR entry created with id ${result.insertedId}`);
+}
+
+async function lookupQRs(client, databaseAndCollection, name, email) {
+  let filter = {name: name,
+                email: email,};
+  const cursor = client.db(databaseAndCollection.db)
+  .collection(databaseAndCollection.collection)
+  .find(filter);
+  const result = await cursor.toArray();
+  return result;
+}
 
 
 
